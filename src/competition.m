0b51f387136058eb9371ad654043814f81fc9138
Charles Pence <charles@charlespence.net>
2010-05-11 15:47:24 +0000

    Added a function to calculate binding in a cooperativity assay.

--- src/competition.m
+++ src/competition.m
@@ -1,7 +1,8 @@
-function [Frac, MTfree, Abound] = MAP_bind(MTtot, Atot, KM, KA)
-% A function which calculates the binding of A to MT assuming that A binds
-% to MT with a disassociation constant of KM and that a second A can bind
-% to an MT-bound A with a disassociation constant of KA
+function [Frac] = competition(MTtot, Atot, Btot, KA, KB)
+% A function which calculates the binding of A to MT assuming a competition
+% assay where A binds to MT with a KD of KA and B binds to MT with a KD of
+% KB. In this competition assay, Atot and MTtot are kept constant while
+% Btot is varied.
 
 % This file is part of MTBindingSim.
 %
@@ -27,21 +28,21 @@ function [Frac, MTfree, Abound] = MAP_bind(MTtot, Atot, KM, KA)
 % - 0.5: Initial version
 
 % Declares symbolic variables
-syms A at mtt ka km
+syms MT mtt at bt ka kb;
 
-% Calculates free and bound A
-A1 = solve(A + (A/km + 2*A^2/(ka*km))*mtt/(1 + A/km + A^2/(km*ka)) - at, A);
-Afree = subs(A1(1), {at mtt km ka}, {Atot MTtot KM KA});
-AB = Atot - Afree;
-Abound = real(AB);
+% Calculates free MT
+MT1 = solve(MT + (1/ka)*MT*at/(1 + (1/ka)*MT) + (1/kb)*MT*bt/(1 + (1/kb)*MT) - mtt, MT);
+MTfree = subs(MT1(1), {mtt at bt ka kb}, {MTtot Atot Btot KA KB});
+
+% Calculates A free
+Afree = Atot./(1 + (1/KA).*MTfree);
+
+% Calculates A bound
+Abound = Atot - Afree;
 
 % Calculates the fraction bound
 F = Abound./Atot;
 Frac = real(F);
-
-% Calculates free MT
-mt = MTtot./(1 + Afree./KM + Afree.^2./(KM*KA));
-MTfree = real(mt);
 
 end
 
