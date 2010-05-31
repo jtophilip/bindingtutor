9a18e317fed0757505b7a36efe3171f2e7cd21c8
Julia Philip <jtophilip@gmail.com>
2010-05-31 16:14:18 +0000

    Improved the comments on cooperativity_binding.

--- src/cooperativity_binding.m
+++ src/cooperativity_binding.m
@@ -2,7 +2,8 @@ function [Frac, MTfree] = cooperativity_binding(MTtot, Atot, KD, P, N)
 % A function which calculates the binding of A to MT assuming cooperative
 % binding where the total concentrations of A and MT are Atot and MTtot,
 % the disassociation constant for the first bound A is KD, and the
-% disassociation constant for the second bound A is KD*P
+% disassociation constant for the second bound A is KD*P for an experiment
+% where [MT] is varied and [A] is held constant.
 
 % This file is part of MTBindingSim.
 %
@@ -27,27 +28,37 @@ function [Frac, MTfree] = cooperativity_binding(MTtot, Atot, KD, P, N)
 % Version history:
 % - 0.5: Initial version
 
+% Determines the size of MTtot and creates an empty vector of the same size
+% for Afree
 [a,b] = size(MTtot);
 Afree = zeros(a,b);
 
-Xguess = MTtot(1);
+% Sets the inital guess for Afree to Atot
+Xguess = Atot;
 
+% Steps through MTtot, calculation Afree at each point
 for n = 1:b
     
+    % Sets up the equation for calcualting Afree and preforms the
+    % calcuation
     f = @(A)A + ((2/KD)*A + (2/(P*(KD^2)))*A^2)*MTtot(n)*N/(1 + (2/KD)*A + (2/(P*(KD^2)))*A^2)-Atot;
     Afree(n) = fzero(f,Xguess);
     
+    % Checks to make sure that fzero sucessfully calculated Afree and ends
+    % calcualation if it did not
     if isnan(Afree(n))
         Frac = 0;
         MTfree = 0;
         return
     end
     
+    % Sets the new guess to the calculated value of AFree
     Xguess = Afree(n);
     n = n +1;
     
 end
 
+% Calculates Abound
 Abound = Atot - Afree;
 
 % Calculates the fraction of A bound
