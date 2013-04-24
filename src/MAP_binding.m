ee358dab93580b3deca6a787ce91048eee613413
Charles Pence <charles@charlespence.net>
2013-04-24 19:31:11 -0400

    Update copyright year.

--- src/MAP_binding.m
+++ src/MAP_binding.m
@@ -6,7 +6,7 @@ function [Frac, MTfree] = MAP_binding(MTtot, Atot, KAM, KAA, N)
 
 % This file is part of MTBindingSim.
 %
-% Copyright (C) 2010-2011  University of Notre Dame
+% Copyright (C) 2010-2013  University of Notre Dame
 %
 % MTBindingSim is free software: you can redistribute it and/or modify
 % it under the terms of the GNU General Public License as published by
--- src/pseudocooperativity_saturation.m
+++ src/pseudocooperativity_saturation.m
@@ -9,7 +9,7 @@ function [Abound, Afree] = pseudocooperativity_saturation(MTtot, Atot, KAM, KAMP
 
 % This file is part of MTBindingSim.
 %
-% Copyright (C) 2010-2011  University of Notre Dame
+% Copyright (C) 2010-2013  University of Notre Dame
 %
 % MTBindingSim is free software: you can redistribute it and/or modify
 % it under the terms of the GNU General Public License as published by
